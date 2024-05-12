173d9d28b5a7c0fe4e973d24c2e30fcaaea53bdb
Md. Kashif Raza <60813449+Kashifraza1208@users.noreply.github.com>
2024-05-12 22:50:25 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -7,13 +7,13 @@ const sendEmail = async (options) => {
         port:process.env.SMTP_PORT,
         service: process.env.SMTP_SERVICE,
         auth: {
-            user: process.env.SMTP_MAIL,
+            user: process.env.SMTP_EMAIL,
             pass: process.env.SMTP_PASSWORD,
         }
     })
 
     const mailOptions = {
-        from: process.env.SMTP_MAIL,
+        from: process.env.SMTP_EMAIL,
         to: options.email,
         subject: options.subject,
         text: options.message,
